822ff993c03770af1f44f0c2c8080b0894f9e509
Levils114 <levils114@gmail.com>
2021-04-06 11:41:08 -0300

    feat: reactivity and state management

--- currency_converter/lib/src/controllers/to_do_controller.dart
+++ currency_converter/lib/src/controllers/to_do_controller.dart
@@ -1,10 +1,21 @@
 import 'package:currency_converter/src/models/to_do_model.dart';
 import 'package:currency_converter/src/repositories/to_do_repository.dart';
+import 'package:flutter/cupertino.dart';
 
 class ToDoController {
   List<ToDoModel> toDos = [];
 
+  final state = ValueNotifier<ToDoState>(ToDoState.start);
+
   Future<void> start() async {
-    toDos = await ToDoRepository().handleGetToDo();
+    state.value = ToDoState.loading;
+    try {
+      toDos = await ToDoRepository().handleGetToDo();
+      state.value = ToDoState.success;
+    } catch (err) {
+      state.value = ToDoState.error;
+    }
   }
 }
+
+enum ToDoState { start, loading, success, error }
--- currency_converter/lib/src/screens/async_programation_page.dart
+++ currency_converter/lib/src/screens/async_programation_page.dart
@@ -1,20 +1,106 @@
+import 'package:currency_converter/src/controllers/to_do_controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class AsyncProgramationPage extends StatelessWidget {
+class AsyncProgramationPage extends StatefulWidget {
+  State createState() {
+    return (AsyncProgramationState());
+  }
+}
+
+class AsyncProgramationState extends State<AsyncProgramationPage> {
+  final controller = ToDoController();
+
+  Widget _success() {
+    return (ListView.builder(
+      itemCount: controller.toDos.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Checkbox(
+            value: controller.toDos[index].completed,
+            onChanged: (value) {},
+          ),
+          title: Text(controller.toDos[index].title),
+        );
+      },
+    ));
+  }
+
+  Widget _error(BuildContext context) {
+    return (Container(
+      color: Colors.redAccent,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("There's an error!", style: TextStyle(color: Colors.white)),
+          SizedBox(
+            height: 8,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                controller.start();
+              },
+              child: Text(
+                'Try Again',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ))
+        ],
+      ),
+    ));
+  }
+
+  Widget _loading() {
+    return (Center(
+      child: CircularProgressIndicator(),
+    ));
+  }
+
+  Widget _start() {
+    return (Container());
+  }
+
+  Widget _stateManagement(
+      ValueNotifier<ToDoState> state, BuildContext context) {
+    switch (state.value) {
+      case ToDoState.start:
+        return _start();
+      case ToDoState.loading:
+        return _loading();
+      case ToDoState.success:
+        return _success();
+      case ToDoState.error:
+        return _error(context);
+      default:
+        return _start();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.start();
+  }
+
   @override
   Widget build(BuildContext context) {
     return (Scaffold(
         appBar: AppBar(
           backgroundColor: Color.fromRGBO(46, 46, 46, 1),
           shadowColor: Color.fromRGBO(46, 46, 46, 1),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.refresh_outlined),
+                onPressed: () {
+                  controller.start();
+                })
+          ],
         ),
-        body: ListView.builder(
-          itemCount: 20,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('neymar $index'),
-            );
+        body: AnimatedBuilder(
+          animation: controller.state,
+          builder: (context, child) {
+            return _stateManagement(controller.state, context);
           },
         )));
   }
